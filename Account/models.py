d855f5d44067c38f1cfe1a037af2830bb403a22e
007 <dr007cc@gmail.com>
2022-02-16 19:12:04 +0330

    init Account Model

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# utils
+from utils.models import file_path, username
+
+
+class AccountManager(models.Manager):
+
+    def submit(self, telegram_id, **kwargs):
+        username = kwargs.get('username') or None
+        photo_url = kwargs.get('photo_url') or None
+
+        first_name = kwargs.get('first_name') or ''
+        last_name = kwargs.get('last_name') or ''
+
+        try:
+            account = self.get(telegram_id=telegram_id)
+
+            # user
+            account.user.first_name = first_name
+            account.user.last_name = last_name
+            account.user.save()
+
+            # account
+            account.username = username
+            account.photo_url = photo_url
+            account.save()
+
+        except self.model.DoesNotExist:
+
+            user = User(first_name=first_name, last_name=last_name)
+            user.save()
+
+            account = Account(
+                user=user,
+                telegram_id=telegram_id,
+                username=username,
+                photo_url=photo_url,
+            )
+            account.save()
+
+        return account
+
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telegram_id = models.PositiveBigIntegerField()
+    username = models.CharField(
+        max_length=64,
+        blank=True,
+        null=True,
+        help_text='Optional',
+    )
+    photo_url = models.URLField(
+        blank=True,
+        null=True,
+        help_text='Optional',
+    )
+    picture = models.ImageField(
+        upload_to=file_path('Account/picture/'),
+        blank=True,
+        null=True,
+        help_text='Optional',
+    )
+    objects = AccountManager()
+
+    def __str__(self):
+        return username(self.user)
