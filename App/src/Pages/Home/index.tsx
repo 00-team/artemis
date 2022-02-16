92b401dc4b600501762460ece58e8363b4db649e
sadra <sadrataghavi1383@gmail.com>
2022-02-16 12:04:51 +0330

    clean coded home page

---
+++ App/src/Pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react'
+
+// style
+import './style/index.scss'
+
+// sections
+import HeroSection from './HeroSection'
+
+const Home: FC = () => {
+    return (
+        <>
+            <HeroSection />
+        </>
+    )
+}
+
+export default Home
