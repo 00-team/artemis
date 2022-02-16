f739c7819f53179f6d68ffdcd196c0f986bd8d8e
sadra <sadrataghavi1383@gmail.com>
2022-02-16 12:02:03 +0330

    home page created

---
+++ App/src/Pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// comps
+import HeroSection from '../components/HeroSection'
+
+const Home = () => {
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    )
+}
+
+export default Home
