34295843a23ad92a2dc0ee15c4899167f2a14790
sadra <sadrataghavi1383@gmail.com>
2022-02-19 20:06:38 +0330

    worked on hero section

--- App/src/Pages/Home/HeroSection.tsx
+++ App/src/Pages/Home/HeroSection.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // style
 import './style/herosection.scss'
 
 const HeroSection = () => {
-    return <div>HeroSection</div>
+    const [ScrollWin, setScrollWin] = useState(0)
+
+    window.onscroll = () => {
+        tranfromHandler()
+    }
+
+    const tranfromHandler = () => {
+        console.log(window.scrollY)
+        setScrollWin(window.scrollY)
+    }
+
+    return (
+        <section className='hero-container'>
+            <div className='stars'>
+                <div id='stars'></div>
+                <div id='stars2'></div>
+                <div id='stars3'></div>
+            </div>
+            <div className='hero-wrapper'>
+                <div className='hero-img-wrapper'></div>
+                <div className='hero-text-wrapper'>
+                    <div className='title'>Welcome To My NFT COLLECTION</div>
+                    {/* <div className='description'>Enjoy</div> */}
+                </div>
+            </div>
+            <div
+                className='circle right'
+                style={{
+                    transform: `scale(${
+                        ScrollWin > 300 ? ScrollWin / 100 : '2.8'
+                    })  translateX(${-ScrollWin / 2}px)`,
+                }}
+            ></div>
+            <div
+                className='circle left'
+                style={{
+                    transform: `scale(${
+                        ScrollWin > 300 ? ScrollWin / 100 : '2.8'
+                    })  translateX(${ScrollWin / 2}px)`,
+                }}
+            ></div>
+            <div className='cursor'></div>
+        </section>
+    )
 }
 
 export default HeroSection
