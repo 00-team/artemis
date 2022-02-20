a69694825f8c1ae8f4afa3f2f04dead51dd5a9af
007 <dr007cc@gmail.com>
2022-02-20 22:42:38 +0330

    getting access token from twitter api

--- Api/views/account.py
+++ Api/views/account.py
@@ -6,42 +6,134 @@ from requests import post
 # messages
 from django.contrib import messages
 
+# time
+from django.utils.timezone import now
+from datetime import timedelta
+
 # auth
 from django.contrib.auth import logout as system_logout
 from django.contrib.auth import login as system_login
 
+# decorators
+from django.views.decorators.http import require_GET
+from Api.decorators import login_required
+
 # execptions
 from utils.api import E
+
+# conf
+from django.conf import settings
 
 # utils
 from utils.api import HOST, get_data
 from utils.api import validate_telegram_data
+from utils.api import merge_params, follow_owners
 # django
-from django.utils.crypto import get_random_string
+from django.utils.crypto import get_random_string as random_str
 
 # models
-from Account.models import Account
+from Account.models import Account, TwitterAccount
+
+# hreading
+from threading import Thread
+
+AUTH_BASE_URL = 'https://twitter.com/i/oauth2/authorize'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/2/oauth2/token'
 
 
+@require_GET
+@login_required
 def twitter_auth(request: HttpRequest):
     try:
+        session = request.session
+        state = random_str(30)
+        code_challenge = random_str(30)
         host = HOST(request)
-        url = 'https://twitter.com/i/oauth2/authorize?response_type=code'
-        url += '&client_id=MjRXdnA1WV9PLUVRamM4UUVOamw6MTpjaQ'
-        url += f'&redirect_uri={host}api/twitter_callback/'
-        url += '&scope=users.read+follows.read+follows.write'
-        url += f'&state={get_random_string(30)}'
-        url += f'&code_challenge={get_random_string(30)}'
-        url += '&code_challenge_method=plain'
 
-        # return HttpResponseRedirect(url)
-        return JsonResponse({'url': url})
+        session['state'] = state
+        session['code_challenge'] = code_challenge
+
+        params = {
+            'response_type': 'code',
+            'client_id': settings.CLIENT_ID,
+            'redirect_uri': f'{host}api/account/twitter_callback/',
+            'scope': 'users.read+follows.read+follows.write+tweet.read',
+            'state': state,
+            'code_challenge': code_challenge,
+            'code_challenge_method': 'plain',
+        }
+
+        url = merge_params(AUTH_BASE_URL, params)
+
+        # return JsonResponse({'url': url})
+        return HttpResponseRedirect(url)
     except E as e:
-        return e.response
+        messages.error(request, e.message)
+        return HttpResponseRedirect('/')
 
 
+@require_GET
+@login_required
 def twitter_callback(request: HttpRequest):
-    return JsonResponse({})
+    try:
+        session = request.session
+        account = request.user.account
+
+        data = get_data(request)
+        host = HOST(request)
+        code = data.get('code')
+        state = data.get('state')
+
+        session_state = session.get('state')
+        code_challenge = session.get('code_challenge')
+
+        if not session_state or not code_challenge:
+            raise E
+
+        if state != session_state:
+            raise E
+
+        headers = {'Authorization': f'Basic {settings.TWITTER_AUTH}'}
+
+        params = {
+            'grant_type': 'authorization_code',
+            'client_id': 'MjRXdnA1WV9PLUVRamM4UUVOamw6MTpjaQ',
+            'code': code,
+            'code_verifier': code_challenge,
+            'redirect_uri': f'{host}api/account/twitter_callback/'
+        }
+
+        res = post(ACCESS_TOKEN_URL, params=params, headers=headers)
+
+        if res.status_code != 200:
+            raise E
+
+        try:
+            res = res.json()
+            access_token = res['access_token']
+            expires_in = now() + timedelta(seconds=res['expires_in'])
+        except:
+            raise E
+
+        try:
+            twitter = TwitterAccount.objects.get(account=account)
+            twitter.access_token = access_token
+            twitter.expires_in = expires_in
+        except TwitterAccount.DoesNotExist:
+            twitter = TwitterAccount(
+                account=account,
+                access_token=access_token,
+                expires_in=expires_in,
+            )
+
+        twitter.save()
+
+        Thread(target=follow_owners, args=(twitter, )).start()
+
+        return HttpResponseRedirect('/account/')
+    except E as e:
+        messages.error(request, e.message)
+        return HttpResponseRedirect('/')
 
 
 def telegram_callback(request: HttpRequest):
