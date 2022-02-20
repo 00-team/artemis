9ce6a0a32b790cebda889f6d6d80b9358ee8d83a
sadra <sadrataghavi1383@gmail.com>
2022-02-20 15:12:34 +0330

    used observer method for new effects

--- App/src/Pages/Home/Collection.tsx
+++ App/src/Pages/Home/Collection.tsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 // style
 import './style/collection.scss'
 
 const Collection = () => {
+    const LazyRef = useRef<HTMLDivElement>(null)
+    const [isIntersecting, setisIntersecting] = useState(false)
+
+    useEffect(() => {
+        if (LazyRef.current && !isIntersecting) {
+            var observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry && entry.isIntersecting) {
+                        setisIntersecting(true)
+                        observer.disconnect()
+                    }
+                },
+                {
+                    threshold: 1,
+                    rootMargin: '-13px',
+                }
+            )
+
+            observer.observe(LazyRef.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [LazyRef])
+
+    useEffect(() => {
+        console.log(isIntersecting)
+    }, [isIntersecting])
+
     return (
         <section className='collection'>
-            <div className='section-title title'>Explore Collections</div>
+            <div
+                className={`section-title title ${
+                    isIntersecting ? 'shown' : ''
+                }`}
+                ref={LazyRef}
+            >
+                <span>Explore Collections</span>
+            </div>
             <input
                 className='search-bar title_small'
                 type='text'
