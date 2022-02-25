450b8650ff82a91fd75bdc451d736143a0e909b7
sadra <sadrataghavi1383@gmail.com>
2022-02-25 21:23:34 +0330

    created url for owners

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -9,6 +9,8 @@ import { useAlert } from 'react-alert'
 // pages
 import Home from './Pages/Home'
 import Account from './Pages/Account'
+// owners page
+import Alien from './Pages/Owners/Alien'
 
 // style
 import './style/base.scss'
@@ -25,6 +27,7 @@ const App: FC = () => {
             <main>
                 <Routes>
                     <Route path='/' element={<Home />} />
+                    <Route path='/owners/alien' element={<Alien />} />
                     <Route path='account' element={<Account />} />
                 </Routes>
             </main>
