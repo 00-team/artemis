b0c9aa1ee8dca738161ca3d7dc803dc706bf5cdf
007 <dr007cc@gmail.com>
2022-02-26 16:26:42 +0330

    adding some tests

---
+++ Api/test.py
@@ -0,0 +1,53 @@
+from django.test import Client
+from django.test import TestCase
+
+from django.utils.timezone import now
+from django.conf import settings
+
+from hashlib import sha256
+import hmac
+
+c = Client()
+PHOTO_URL = 'https://cdn.discordapp.com/attachments/731174051170746500/836494236941680650/cock.png'
+
+
+class AccountTestCase(TestCase):
+    base_url = '/api/account/'
+
+    def test_telegram_callback(self):
+        now_date = int(now().timestamp())
+
+        data = {
+            'id': 7,
+            'first_name': 'master',
+            'username': 'Master67',
+            'auth_date': now_date,
+            'photo_url': PHOTO_URL
+        }
+
+        data_list = list(map(lambda i: f'{i[0]}={i[1]}', data.items()))
+        data_list.sort()
+
+        data_message = '\n'.join(data_list).encode()
+
+        token = sha256(settings.BOT_TOKEN.encode()).digest()
+
+        hashed = hmac.new(token, msg=data_message, digestmod=sha256)
+        hashed = hashed.hexdigest()
+
+        data['hash'] = hashed
+
+        response = c.get(
+            self.base_url + 'telegram_callback/',
+            follow=True,
+            data=data,
+        )
+
+        self.assertEqual(response.wsgi_request.path, '/account/')
+
+        response = c.get(
+            self.base_url + 'telegram_callback/',
+            follow=True,
+        )
+
+        self.assertEqual(response.wsgi_request.path, '/')
--- Collection/tests.py
+++ Collection/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.files import File
+from django.conf import settings
+
+from .models import Owner
+
+owner_picture = File(open(settings.BASE_DIR / 'static/img/00-Team.png', 'rb'))
+master_wallet = '0x7aE0A149Ce992145078b6E44091fec5358E7AE9A'
+
+
+class OwnerTestCase(TestCase):
+
+    def setUp(self):
+        Owner.objects.create(
+            username='Master',
+            picture=owner_picture,
+            wallet=master_wallet,
+            description='Master is Grate',
+            floor_price='0.1',
+            ceil_price='1',
+        )
+
+    def test_owners(self):
+        '''Owners Test'''
+        master = Owner.objects.get(username='Master')
+        self.assertEqual(master.wallet, master_wallet)
+        master.delete()
