62eee154ee15c4e4b4d923f14b9cb2137a2eac30
sadra <sadrataghavi1383@gmail.com>
2022-02-27 20:09:00 +0330

    wrapper for owner prev card

--- App/src/Pages/Home/Owners.tsx
+++ App/src/Pages/Home/Owners.tsx
@@ -3,6 +3,9 @@ import React, { useEffect, useRef, useState } from 'react'
 // style
 import './style/owners.scss'
 
+// comps
+import OwnerPreviewCard from '../../components/OwnerPreviewCard'
+
 const Owners = () => {
     const LazyRef = useRef<HTMLDivElement>(null)
     const [isIntersecting, setisIntersecting] = useState(false)
@@ -38,6 +41,26 @@ const Owners = () => {
                 ref={LazyRef}
             >
                 <span>See Our Owners</span>
+            </div>
+            <div className='owners-wrapper'>
+                <OwnerPreviewCard
+                    name='Alien'
+                    detail='Tell Me Whats The Secret Of Love I Dont Get It'
+                    image='https://i.guim.co.uk/img/media/ef8492feb3715ed4de705727d9f513c168a8b196/37_0_1125_675/master/1125.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=cb647d991d8897cc8a81d2c33c4406d5'
+                    top_img='https://cdn.discordapp.com/avatars/663056543666667532/363040ba873c85a501ac9b1579958dcb.webp?size=128'
+                />
+                <OwnerPreviewCard
+                    name='Alien'
+                    detail='Tell Me Whats The Secret Of Love I Dont Get It'
+                    image='https://i.guim.co.uk/img/media/ef8492feb3715ed4de705727d9f513c168a8b196/37_0_1125_675/master/1125.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=cb647d991d8897cc8a81d2c33c4406d5'
+                    top_img='https://cdn.discordapp.com/avatars/663056543666667532/363040ba873c85a501ac9b1579958dcb.webp?size=128'
+                />
+                <OwnerPreviewCard
+                    name='Alien'
+                    detail='Tell Me Whats The Secret Of Love I Dont Get It'
+                    image='https://i.guim.co.uk/img/media/ef8492feb3715ed4de705727d9f513c168a8b196/37_0_1125_675/master/1125.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=cb647d991d8897cc8a81d2c33c4406d5'
+                    top_img='https://cdn.discordapp.com/avatars/663056543666667532/363040ba873c85a501ac9b1579958dcb.webp?size=128'
+                />
             </div>
         </section>
     )
