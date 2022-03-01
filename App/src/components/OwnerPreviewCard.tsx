beb6477f15a7e0981a71d9d39edd642cb9007968
sadra <sadrataghavi1383@gmail.com>
2022-03-01 22:25:46 +0330

    added a see more btn for card

--- App/src/components/OwnerPreviewCard.tsx
+++ App/src/components/OwnerPreviewCard.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 
 // style
-import './style/OwnerPreviewCard.scss'
+import './style/ownerpreviewcard.scss'
 
 interface OwnerPreviewCardProps {
     top_img: string
@@ -18,20 +18,23 @@ const OwnerPreviewCard: FC<OwnerPreviewCardProps> = ({
     name,
 }) => {
     return (
-        <div className='owner-prev-card'>
-            <div className='owner-prev-top-img'>
-                <img src={top_img} alt='' />
-            </div>
-            <div className='owner-prev-image'>
-                <img src={image} alt='' />
-            </div>
-            <div className='owner-prev-details'>
-                <div className='owner-name title_small'>
-                    <span>{name}</span>
+        <span className='owner-prev-container'>
+            <div className='owner-prev-card'>
+                <div className='owner-prev-top-img'>
+                    <img src={top_img} alt='' />
                 </div>
-                <div className='owner-detail title_smaller'>{detail}</div>
+                <div className='owner-prev-image'>
+                    <img src={image} alt='' />
+                </div>
+                <div className='owner-prev-details'>
+                    <div className='owner-name title_small'>
+                        <span>{name}</span>
+                    </div>
+                    <div className='owner-detail title_smaller'>{detail}</div>
+                    <button className='see-more title_smaller'>see more</button>
+                </div>
             </div>
-        </div>
+        </span>
     )
 }
 
