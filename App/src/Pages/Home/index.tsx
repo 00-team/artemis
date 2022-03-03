640e30ae793445fdc3f7bf7652b70f3a986c69a5
sadra <sadrataghavi1383@gmail.com>
2022-03-03 18:57:34 +0330

    linked new section to home page

--- App/src/Pages/Home/index.tsx
+++ App/src/Pages/Home/index.tsx
@@ -6,12 +6,14 @@ import './style/index.scss'
 // sections
 import HeroSection from './HeroSection'
 import Owners from './Owners'
+import Faq from './Faq'
 
 const Home: FC = () => {
     return (
         <main>
             <HeroSection />
             <Owners />
+            <Faq />
         </main>
     )
 }
