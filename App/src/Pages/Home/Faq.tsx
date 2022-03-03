5b652b5a4a177608a831bb98c2ce5b0a510c4553
sadra <sadrataghavi1383@gmail.com>
2022-03-03 19:18:38 +0330

    clean code/used new comp

--- App/src/Pages/Home/Faq.tsx
+++ App/src/Pages/Home/Faq.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
+import UnderlineText from '../../components/UnderlineText'
+
 // style
 import './style/faq.scss'
 
 const Faq = () => {
     return (
         <div className='faq-container'>
-            <div className='section-title title'></div>
+            <UnderlineText>Owners Faq</UnderlineText>
             <div className='faq-wrapper'></div>
         </div>
     )
