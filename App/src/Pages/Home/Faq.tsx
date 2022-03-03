c79734ba9711c889853e8d122d921e960877355b
sadra <sadrataghavi1383@gmail.com>
2022-03-03 18:54:23 +0330

    initiated new section 'faq'

---
+++ App/src/Pages/Home/Faq.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// style
+import './style/faq.scss'
+
+const Faq = () => {
+    return (
+        <div className='faq-container'>
+            <div className='section-title title'></div>
+            <div className='faq-wrapper'></div>
+        </div>
+    )
+}
+
+export default Faq
