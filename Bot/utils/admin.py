90112befdc7a97c0e0b2d1e5da9965d2846cf6a8
007 <dr007cc@gmail.com>
2022-03-27 13:21:36 +0430

    updating admin file into new data system

--- Bot/utils/admin.py
+++ Bot/utils/admin.py
@@ -1,38 +1,28 @@
 # types
-from telegram import Chat, Update
+from telegram import Update
 from telegram.ext import CallbackContext
 
-# exceptions
-from telegram.error import BadRequest, Unauthorized
+# decorators
+from .decorators import user_data, require_admin
 
 # data
 from .data import markdown_free
 
-# conf
-from .config import HOST
-
 # stages
 from .stages import user_joined
 
-# requests
-import requests
 
-API_URL = HOST + 'api/bot/'
-
-USER_PHOTO = 'AgACAgQAAxkBAAIB1mIQ-B9bGKi7MRGKh9Oqhwi1JWpkAALMuTEbwMCIUGuGyh16ARp-AQADAgADbQADIwQ'
-
-
-def photo_info(update: Update, *args):
-    user = update.effective_user
-
-    if not user.id in admins:
-        return
-
+@user_data
+@require_admin
+def photo_info(update: Update, **kwargs):
     photos = update.effective_message.photo
     chat = update.effective_chat
 
     for photo in photos:
-        caption = f'''file id: `{photo.file_id}`\n\nfile size: `{photo.file_size}`\n\width: {photo.width}\t\theight: {photo.height}'''
+        caption = (f'file id: \n`{photo.file_id}`\n\n'
+                   f'file size: `{photo.file_size // 1000} KB`\n'
+                   f'width: {photo.width}\n'
+                   f'height: {photo.height}')
         chat.send_photo(
             photo.file_id,
             caption=caption,
@@ -40,43 +30,11 @@ def photo_info(update: Update, *args):
         )
 
 
-def get_user_status(user_id) -> str:
-    BOT_SECRET = get_token()[1]
-
-    headers = {'Authorization': BOT_SECRET}
-
-    params = {'user_id': user_id}
-
-    try:
-        response = requests.get(
-            API_URL + 'user_status/',
-            params=params,
-            headers=headers,
-        ).json()
-
-        if response.get('error'):
-            return 'site: ' + response['error'] + ' ❌'
-
-        twitter = response['twitter']
-
-        if twitter:
-            twitter = f'[{twitter}](https://twitter.com/{twitter}) Connected ✅'
-        else:
-            twitter = 'Disconnected ❌'
-
-        return ('site: Loged in ✅\n'
-                f'wallet: `{markdown_free(response["wallet"])}`\n'
-                f'twitter: {twitter}')
-    except:
-        return 'Error to get update from website'
-
-
-def view_user(update: Update, context: CallbackContext):
+@user_data
+@require_admin
+def view_user(update: Update, context: CallbackContext, **kwargs):
     user_admin = update.effective_user
     bot = user_admin.bot
-
-    if user_admin.id not in admins:
-        return
 
     try:
         user_id = int(context.args[0])
@@ -86,23 +44,10 @@ def view_user(update: Update, context: CallbackContext):
             '/user <user-id:number>\n'
             '/user 12', )
 
-    user_data = user_by_id(user_id)
-
-    if not user_data:
-        return user_admin.send_message(
-            f'user with id {user_id} was not found!')
-
     try:
         user = bot.get_chat(user_id)
     except:
         return user_admin.send_message('Error to get this chat')
-
-    # try:
-    #     user.send_message('test message from admin')
-    # except Unauthorized:
-    #     return user_admin.send_message('bot was blocked by the user')
-
-    # photo =
 
     def chats():
         not_joined = user_joined(user)
@@ -117,11 +62,19 @@ def view_user(update: Update, context: CallbackContext):
     fname = markdown_free(f'full name: {user.full_name}')
     uname = f'username: [{user.username}]({user.link})'
     bio = markdown_free(f'bio: {user.bio}')
-    lang = f'lang: {user_data.lang}'
-    invites = f'invites: {user_data.total_invites}'
-    uchats = chats()
-    ustatus = get_user_status(user_id)
 
-    text = '\n'.join((fname, uname, bio, lang, invites, uchats, ustatus))
+    uchats = chats()
+
+    text = '\n'.join((fname, uname, bio, uchats))
 
     user_admin.send_message(text, parse_mode='MarkdownV2')
+
+
+@user_data
+@require_admin
+def admin_help(update: Update, **kwargs):
+    chat = update.effective_chat
+    text = ('/user <user:id> - for viewing the users\n'
+            '/admin - for showing this message\n'
+            'sending or forwarding a photo - getting info about that photo')
+    chat.send_message(text)
