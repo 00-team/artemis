d38b0827fd25e27392c6b850de316b00c2b6618c
007 <dr007cc@gmail.com>
2022-03-27 17:54:46 +0430

    making separate a file for keyboards

---
+++ Bot/utils/keyboards.py
@@ -0,0 +1,50 @@
+from telegram import LoginUrl, Chat
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+# conf
+from .config import HOST
+
+# langs
+from .langs import CONTNET
+
+LOGIN_URL = LoginUrl(HOST + 'api/account/telegram_callback/')
+
+
+def help_keyboard(lang):
+    cboard = CONTNET[lang]['help_keyboard']
+    keyboard = list(
+        map(
+            lambda line: list(map(
+                lambda b: InlineKeyboardButton(**b),
+                line,
+            )),
+            cboard,
+        ))
+    return InlineKeyboardMarkup(keyboard)
+
+
+def login_keyboard(lang):
+    text = CONTNET[lang]['login_button']
+    keyboard = [[InlineKeyboardButton(text, login_url=LOGIN_URL)]]
+
+    return InlineKeyboardMarkup(keyboard)
+
+
+def join_keyboard(chats: list[Chat], lang):
+    text = text = CONTNET[lang]['chats_check_button']
+
+    def GKB(c: Chat):
+        return [InlineKeyboardButton(c.title, url=c.invite_link)]
+
+    keyboard = list(map(GKB, chats)) + [[
+        InlineKeyboardButton(text, callback_data='check_chats')
+    ]]
+
+    return InlineKeyboardMarkup(keyboard)
+
+
+def invite_keyboard(link: str, lang):
+    text = CONTNET[lang]['invite_button']
+    keyboard = [[InlineKeyboardButton(text, url=link)]]
+
+    return InlineKeyboardMarkup(keyboard)
