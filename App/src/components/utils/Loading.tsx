d8997cc65495f6b7737de3fc296e5a241e3c3a8c
sadra <sadrataghavi1383@gmail.com>
2022-04-01 16:16:49 +0430

    created a loading component

---
+++ App/src/components/utils/Loading.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react'
+
+// style
+import './style/loading.scss'
+
+interface LoadingProps {
+    message: string
+}
+
+const Loading: FC<LoadingProps> = ({ message }) => {
+    return (
+        <div className='loading-container'>
+            <div className='loading'>
+                <div className='loading__square'></div>
+                <div className='loading__square'></div>
+                <div className='loading__square'></div>
+                <div className='loading__square'></div>
+                <div className='loading__square'></div>
+                <div className='loading__square'></div>
+                <div className='loading__square'></div>
+            </div>
+            <div className='title_small'>{message}</div>
+        </div>
+    )
+}
+
+export default Loading
