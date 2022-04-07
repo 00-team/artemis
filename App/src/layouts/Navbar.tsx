06e3dd324769f4abb32ca35a5f7a99f521ff659b
sadra <sadrataghavi1383@gmail.com>
2022-04-07 19:42:09 +0430

    wrote navbar basic jsx and states improvided

--- App/src/layouts/Navbar.tsx
+++ App/src/layouts/Navbar.tsx
@@ -1,17 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 // style
 import './style/navbar.scss'
 
+// icons
+import { AiFillHome } from '@react-icons/all-files/ai/AiFillHome'
+import { FaWallet } from '@react-icons/all-files/fa/FaWallet'
+
 const Navbar = () => {
     let location = useLocation()
+
+    const [MenuActive, setMenuActive] = useState(false)
 
     useEffect(() => {
         console.log(location)
     }, [location])
 
-    return <div>Navbar</div>
+    return (
+        <div className='navbar-container'>
+            <div
+                onClick={() => setMenuActive(!MenuActive)}
+                className={`menu ${MenuActive ? 'active' : ''}`}
+            >
+                <div className='toggle'></div>
+
+                <li>
+                    <div className='first'>
+                        <FaWallet size={25} />
+                    </div>
+                </li>
+                <li>
+                    <div className='sec'>
+                        <AiFillHome size={25} />
+                    </div>
+                </li>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
