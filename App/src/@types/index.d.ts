fecf2adbc6b1920c233401c37781aedda090410b
sadra <sadrataghavi1383@gmail.com>
2022-04-08 17:23:35 +0430

    declared a module in order to add custom html attrs in ts

--- App/src/@types/index.d.ts
+++ App/src/@types/index.d.ts
@@ -4,3 +4,10 @@ import { AlertManager } from 'react-alert'
 declare global {
     var ReactAlert: AlertManager
 }
+
+declare module 'react' {
+    interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+        // extends React's HTMLAttributes
+        custom?: string
+    }
+}
