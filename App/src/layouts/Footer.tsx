5372461623b35bc83aa764b3c78f36c0f1ae39c5
sadra <sadrataghavi1383@gmail.com>
2022-04-08 21:20:54 +0430

    completed footer for large screen size gotta work on responsive

--- App/src/layouts/Footer.tsx
+++ App/src/layouts/Footer.tsx
@@ -5,46 +5,67 @@ import React from 'react'
 // style
 import './style/footer.scss'
 
+const SiteLogo = require('../../../static/img/00-Team.png')
+
 const Footer = () => {
     return (
         <div className='footer-container'>
             <div className='footer-wrapper'>
-                <div className='footer-column'>
-                    <div className='start title_small'>NightCurly</div>
-                    <a
-                        data-text='&nbsp;home'
-                        href=''
-                        className='link title_smaller'
-                    >
-                        &nbsp;home
-                    </a>
-                    <a
-                        data-text='&nbsp;FAQ'
-                        href=''
-                        className='link title_smaller'
-                    >
-                        &nbsp;FAQ
-                    </a>
-                    <a
-                        data-text='&nbsp;Owners'
-                        href=''
-                        className='link title_smaller'
-                    >
-                        &nbsp;Owners
-                    </a>
+                <div className='footer-columns'>
+                    <div className='footer-column'>
+                        <div className='start title_small'>NightCurly</div>
+                        <a
+                            data-text='&nbsp;home'
+                            href=''
+                            className='link title_smaller'
+                        >
+                            &nbsp;home
+                        </a>
+                        <a
+                            data-text='&nbsp;FAQ'
+                            href=''
+                            className='link title_smaller'
+                        >
+                            &nbsp;FAQ
+                        </a>
+                        <a
+                            data-text='&nbsp;Owners'
+                            href=''
+                            className='link title_smaller'
+                        >
+                            &nbsp;Owners
+                        </a>
+                    </div>
+                    <div className='footer-column'>
+                        <div className='start title_small'>community</div>
+                        <a
+                            data-text='&nbsp;disocrd'
+                            href=''
+                            className='link title_smaller'
+                        >
+                            &nbsp;discord
+                        </a>
+                    </div>
                 </div>
-                <div className='footer-column'>
-                    <div className='start title_small'>community</div>
-                    <a
-                        data-text='&nbsp;disocrd'
-                        href=''
-                        className='link title_smaller'
-                    >
-                        &nbsp;discord
+
+                <div className='footer-logo'>
+                    <a href='/'>
+                        <img
+                            src={SiteLogo}
+                            alt='siteLogo site-logo site_logo'
+                        />
                     </a>
+                    <div className='footer-description description'>
+                        ©2022 NightCurly
+                    </div>
                 </div>
             </div>
-            <div className='footer-logo'></div>
+            <div className='creator'>
+                Created With ❤ By:
+                <a href='https://web-00-team.web.app/'>
+                    00 Team © {new Date().getFullYear()}
+                </a>
+            </div>
         </div>
     )
 }
