3a3218e19bb221591e6c1b6722cabba8d6832115
sadra <sadrataghavi1383@gmail.com>
2022-04-08 17:26:25 +0430

    created footer and worked on its design which turned out to be Fabulous

---
+++ App/src/layouts/Footer.tsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+// utils
+
+// style
+import './style/footer.scss'
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <div className='footer-wrapper'>
+                <div className='footer-column'>
+                    <div className='start title_small'>NightCurly</div>
+                    <a
+                        data-text='&nbsp;home'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;home
+                    </a>
+                    <a
+                        data-text='&nbsp;test'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;test
+                    </a>
+                    <a
+                        data-text='&nbsp;test'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;test
+                    </a>
+                </div>
+                <div className='footer-column'>
+                    <div className='start title_small'>test</div>
+                    <a
+                        data-text='&nbsp;test'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;test
+                    </a>
+                    <a
+                        data-text='&nbsp;test'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;test
+                    </a>
+                    <a
+                        data-text='&nbsp;test'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;test
+                    </a>
+                </div>
+                <div className='footer-column'>
+                    <div className='start title_small'>community</div>
+                    <a
+                        data-text='&nbsp;test'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;test
+                    </a>
+                    <a
+                        data-text='&nbsp;test'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;test
+                    </a>
+                    <a
+                        data-text='&nbsp;test'
+                        href=''
+                        className='link title_smaller'
+                    >
+                        &nbsp;test
+                    </a>
+                </div>
+            </div>
+            <div className='footer-logo'></div>
+        </div>
+    )
+}
+
+export default Footer
