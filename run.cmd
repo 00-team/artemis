f15f184bec8acddb9439b3576c607a33084f2ace
007 <dr007cc@gmail.com>
2022-04-10 11:02:28 +0430

    clearing the run script

--- run.cmd
+++ run.cmd
@@ -1,6 +1,9 @@
 @echo off
+
 :: set night curly mode in environment variables as dev
 :: there is only DEV and BUILD values are exists
 set NIGHTCURLY_MODE=DEV
+:: set NIGHTCURLY_MODE=BUILD
+
 :: runing the django project
-python manage.py runserver 7000 --insecure
+python manage.py runserver 7000
