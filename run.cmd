1a4b01b2ecb34a049713bd8f6fbab2a2a57c97e0
007 <dr007cc@gmail.com>
2022-04-10 09:54:46 +0430

    adding a script for runnign the django project

---
+++ run.cmd
@@ -0,0 +1,6 @@
+@echo off
+:: set night curly mode in environment variables as dev
+:: there is only DEV and BUILD values are exists
+set NIGHTCURLY_MODE=DEV
+:: runing the django project
+python manage.py runserver 7000 --insecure
