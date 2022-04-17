432dedd6af0a44bd03f869f0ac03c9ce1a4b2bf3
007 <dr007cc@gmail.com>
2022-04-17 23:23:13 +0430

    adding a cmd run script for bot

---
+++ Bot/run.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+:: set night curly mode in environment variables as dev
+:: there is only DEV and BUILD values are exists
+set NIGHTCURLY_MODE=DEV
+:: set NIGHTCURLY_MODE=BUILD
+
+:: runing the bot
+python run.py
