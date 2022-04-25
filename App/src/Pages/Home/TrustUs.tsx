1fc51601e37a15e4ccd1ceeac7653924ef2c3610
sadra <sadrataghavi1383@gmail.com>
2022-04-25 16:48:05 +0430

    imported redux in order to get info from back-end with redux

--- App/src/Pages/Home/TrustUs.tsx
+++ App/src/Pages/Home/TrustUs.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+// redux
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState, UpdateGeneralInfo } from 'state'
+
+// utils
+import { DisplayNumbers } from '@00-team/utils'
+
+const _ = (n: number) => DisplayNumbers(n, 10e3, 10e9)
 
 // style
 import './style/trustus.scss'
 
 const TrustUs = () => {
-    return <div>TrustUs</div>
+    const dispatch = useDispatch()
+    const GeneralInfo = useSelector((s: RootState) => s.GeneralInfo)
+
+    useEffect(() => {
+        dispatch(UpdateGeneralInfo())
+        let interval = setInterval(() => dispatch(UpdateGeneralInfo()), 7000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [dispatch])
+
+    return (
+        <section className='trustus-container'>
+            <span>bot users: {_(GeneralInfo.bot_users)}</span>
+            <span>accounts: {_(GeneralInfo.accounts)}</span>
+            <span>twitters: {_(GeneralInfo.twitters)}</span>
+        </section>
+    )
 }
 
 export default TrustUs
