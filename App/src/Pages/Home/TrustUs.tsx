a8df48f4a579735da6f7c02082012bb96ad455cb
sadra <sadrataghavi1383@gmail.com>
2022-04-27 17:25:07 +0430

    finished trust us section

--- App/src/Pages/Home/TrustUs.tsx
+++ App/src/Pages/Home/TrustUs.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 // redux
 import { useDispatch, useSelector } from 'react-redux'
@@ -7,6 +7,7 @@ import { RootState, UpdateGeneralInfo } from 'state'
 // utils
 import { DisplayNumbers } from '@00-team/utils'
 import UnderlineText from 'components/utils/UnderlineText'
+import CountUpAnim from 'components/utils/CountUpAnim'
 
 // style
 import './style/trustus.scss'
@@ -16,6 +17,30 @@ const _ = (n: number) => DisplayNumbers(n, 10e3, 10e9)
 const TrustUs = () => {
     const dispatch = useDispatch()
     const GeneralInfo = useSelector((s: RootState) => s.GeneralInfo)
+
+    const LazyRef = useRef<HTMLDivElement>(null)
+    const [isIntersecting, setisIntersecting] = useState(false)
+
+    useEffect(() => {
+        if (LazyRef.current && !isIntersecting) {
+            var observer = new IntersectionObserver(
+                ([entry]) => {
+                    if (entry && entry.isIntersecting) {
+                        setisIntersecting(true)
+                        observer.disconnect()
+                    }
+                },
+                {
+                    rootMargin: window.innerWidth < 768 ? '-35px' : '-70px',
+                }
+            )
+
+            observer.observe(LazyRef.current)
+        }
+        return () => {
+            if (observer) observer.disconnect()
+        }
+    }, [LazyRef])
 
     useEffect(() => {
         dispatch(UpdateGeneralInfo())
@@ -39,9 +64,29 @@ const TrustUs = () => {
     return (
         <section className='trustus-container' id='trust'>
             <UnderlineText threshold={1}>Why Trust Us?</UnderlineText>
-            <span>bot users: {_(GeneralInfo.bot_users)}</span>
-            <span>accounts: {_(GeneralInfo.accounts)}</span>
-            <span>twitters: {_(GeneralInfo.twitters)}</span>
+            <div className='trust-wrapper' ref={LazyRef}>
+                <div className={`trust-item ${isIntersecting ? 'active' : ''}`}>
+                    <div className='holder'>Users</div>
+                    <CountUpAnim
+                        end={Number(_(GeneralInfo.bot_users))}
+                        speed={50}
+                    />
+                </div>
+                <div className={`trust-item ${isIntersecting ? 'active' : ''}`}>
+                    <div className='holder'>Customers</div>
+                    <CountUpAnim
+                        end={Number(_(GeneralInfo.accounts))}
+                        speed={50}
+                    />
+                </div>
+                <div className={`trust-item ${isIntersecting ? 'active' : ''}`}>
+                    <div className='holder'>Twitters</div>
+                    <CountUpAnim
+                        end={Number(_(GeneralInfo.twitters))}
+                        speed={50}
+                    />
+                </div>
+            </div>
         </section>
     )
 }
