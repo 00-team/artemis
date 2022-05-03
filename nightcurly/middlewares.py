6f0212c4f47f504ab35551dfef052abab57c8949
007 <dr007cc@gmail.com>
2022-05-03 20:40:47 +0430

    updating hit counter

--- nightcurly/middlewares.py
+++ nightcurly/middlewares.py
@@ -2,12 +2,18 @@ from Collection.models import HitCount
 from django.http import HttpRequest
 
 
+DISALLOWED_PATHS = ('/api', '/favicon.ico', '/admin', '/robots.txt')
+
+
 class HitCountMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request: HttpRequest):
         response = self.get_response(request)
+
+        if request.path.startswith(DISALLOWED_PATHS):
+            return response
 
         if not request.session.get('hit_counted'):
             hit_count = HitCount.load()
