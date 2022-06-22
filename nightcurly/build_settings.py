0be8e0ff7680bafe8df89525e05b208075854753
007 <dr007cc@gmail.com>
2022-06-22 21:55:36 +0430

    using mariadb for build

--- nightcurly/build_settings.py
+++ nightcurly/build_settings.py
@@ -1,10 +1,26 @@
 from .settings import *
 
+
 MODE = 'BUILD'
 DEBUG = False
 
 ALLOWED_HOSTS = SECRETS.data[SECRETS.mode]['ALLOWED_HOSTS']
 
 del STATICFILES_DIRS
+
+
+BASE_DB = {
+    'ENGINE': 'django.db.backends.mysql',
+    'PORT': 3306,
+    'USER': SECRETS.DB_USER,
+    'PASSWORD': SECRETS.DB_PASS,
+}
+
+DATABASES = {
+    'default': {
+        'NAME': 'nightcurly_default',
+        **BASE_DB
+    },
+}
 
 STATIC_ROOT = BASE_DIR / 'static'
