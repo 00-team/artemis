550975dc9c3f903e7520c9191b718abd0f7ca91b
007 <dr007cc@gmail.com>
2022-07-10 12:09:27 +0430

    chaning random-username prefix to RU

--- Account/signals/user.py
+++ Account/signals/user.py
@@ -1,12 +1,7 @@
-# signals
+from django.contrib.auth.models import User
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-
-# utils
 from django.utils.crypto import get_random_string
-
-# models
-from django.contrib.auth.models import User
 
 
 # random username
@@ -14,7 +9,7 @@ from django.contrib.auth.models import User
 def user_pre_save(instance, **kwargs):
     if not instance.username:
         while True:
-            username = 'random_user-' + get_random_string(32)
+            username = 'RU-' + get_random_string(32)
             if User.objects.filter(username=username).exists():
                 continue
             else:
